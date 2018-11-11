a5cdae1e5b0b50a30789dbe38d66bfe25d2caa92
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2018-11-11 01:44:58 -0500

    code written - weponclash function is not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,8 +27,15 @@ var playernumber = 0;
 var p1 = null;
 var p2 = null;
 var p1name;
+var p2name;
 var p1arr = [];
 var p2arr = [];
+var playerOneWinCount = 0;
+var playerOneLossCount = 0;
+var playerTwoWinCount = 0;
+var playerTwoLossCount = 0;
+var p1Weapon = null;
+var p2Weapon = null;
 
 
 //player connection-stops if disconnected
@@ -71,13 +78,18 @@ function p2SnapshotToArray(snapshot) {
 
 //check if player1 exists - for persistance on other pages
 player1.on("value", function(snapshot) {
-  
+
   if (snapshot.child("/name").exists() === true){
       p1SnapshotToArray(snapshot)
       console.log(p1arr);
       $(".player1Name").html(p1arr[1]);
       p1 = p1arr[1];
-      }
+      $("#p1Wins").html(p1arr[2]);
+      $("#p1loss").html(p1arr[0]);
+    } else {
+      $("#messageText").text("Enter your name above to join the Fight!")
+    }
+      player1.onDisconnect().remove();
     });
 
 //check if player2 exists - for persistance on other pages
@@ -88,8 +100,12 @@ player1.on("value", function(snapshot) {
           console.log(p2arr);
           $(".player2Name").html(p2arr[1]);
           p1 = p2arr[1];
+          $("#p2Wins").html(p2arr[2]);
+          $("#p2loss").html(p2arr[0]);
           noMorePlayers();
+          $("#messageText").text("Select your Weapons!");
           }
+          player2.onDisconnect().remove();
         });
 
 //function to prevent a third player from joining
@@ -115,28 +131,196 @@ $("#joinFight").on("click",function(event){
     p1 = playerName;
     player1.set({
       name: playerName,
-      wins: 0,
-      loss: 0,
+      wins: playerOneWinCount,
+      loss: playerOneLossCount,
+      zweapon: p1Weapon
     });
     sessionStorage.setItem("player number", "1");
     $(".player1Name").html(playerName);
+    $("#messageText").text("Waiting for an opponent!");
+    $("#playerRegister").remove();
     noMorePlayers()
+    player1.onDisconnect().remove();
   } else if (p2 == null) {
+    p2name = $("#inputName").val().trim();
     p2 = playerName
     player2.set({
       name: playerName,
-      wins: 0,
-      loss: 0,
+      wins: playerTwoWinCount,
+      loss: playerTwoLossCount,
+      zweapon: p2Weapon
     });
     sessionStorage.setItem("player number", "2");
     $(".player2Name").html(playerName);
-    noMorePlayers()
+    noMorePlayers();
+    player2.onDisconnect().remove();
   }
 
 
 
 });
 
+//p1 wins function
+function p1Win(){
+  playerOneWinCount ++;
+  $("#p1Wins").html(playerOneWinCount);
+  $("#messageText").html("Player 1 Wins!");
+}
+
+//p1 wins function
+function p1loss(){
+  playerOneLossCount ++;
+  $("#p1loss").html(playerOneLossCount);
+  $("#messageText").html("Player 2 Wins!");
+}
+
+//p2 wins function
+function p2Win(){
+  playerTwoWinCount ++;
+  $("#p2Wins").html(playerTwoWinCount);
+  $("#messageText").html("Player 2 Wins!");
+
+}
+
+//p2 loss function
+function p2loss(){
+  playerTwoLossCount ++;
+  $("#p2loss").html(playerTwoLossCount);
+  $("#messageText").html("Player 1 Wins!");
+}
+
+//tied game function
+function tie(){
+  $("#messageText").html("This round is a tie");
+}
+
+
+//weapon clicks
+$(document).on("click", "#sword1", function(){
+    p1Weapon = "Sword";
+    player1.set({
+        Name: p1name,
+        Wins: playerOneWinCount,
+        Losses: playerOneLossCount,
+        zweapon: p1Weapon
+    });
+    $("#messageText").html("Waiting for Player 2!");
+    weaponClash ();
+
+});
+
+$(document).on("click", "#axe1", function(){
+    p1Weapon = "Axe";
+    player1.set({
+        Name: p1name,
+        Wins: playerOneWinCount,
+        Losses: playerOneLossCount,
+        zweapon: p1Weapon
+    });
+    $("#messageText").html("Waiting for Player 2!");
+    weaponClash ();
+
+});
+
+$(document).on("click", "#lance1", function(){
+    p1Weapon = "Lance";
+    player1.set({
+        Name: p1name,
+        Wins: playerOneWinCount,
+        Losses: playerOneLossCount,
+        zweapon: p1Weapon
+    });
+    $("#messageText").html("Waiting for Player 2!");
+    weaponClash ();
+
+});
+
+$(document).on("click", "#sword2", function(){
+    p2Weapon = "Sword";
+    player2.set({
+        Name: p2name,
+        Wins: playerTwoWinCount,
+        Losses: playerTwoLossCount,
+        zweapon: p2Weapon
+    });
+    $("#messageText").html("Waiting for Player 1!");
+    weaponClash ();
+
+
+});
+
+$(document).on("click", "#axe2", function(){
+    p2Weapon = "Axe";
+    player2.set({
+        Name: p2name,
+        Wins: playerTwoWinCount,
+        Losses: playerTwoLossCount,
+        zweapon: p2Weapon
+    });
+    $("#messageText").html("Waiting for Player 1!");
+    weaponClash ();
+
+});
+
+$(document).on("click", "#lance2", function(){
+    p2Weapon = "Lance";
+    player2.set({
+        Name: p2name,
+        Wins: playerTwoWinCount,
+        Losses: playerTwoLossCount,
+        zweapon: p2Weapon
+    });
+    $("#messageText").html("Waiting for Player 1!");
+    weaponClash ();
+
+});
+
+//lots of if statements to check winner
+function weaponClash (){
+  if (p1Weapon === "Sword" && p2Weapon === "Sword") {
+          tie();
+
+      }
+
+      else if (p1Weapon === "Sword" && p2Weapon === "Axe") {
+          p1Win();
+          p2loss();
+      }
+
+      else if (p1Weapon === "Sword" && p2Weapon === "Lance") {
+          p2Win();
+          p1loss();
+      }
+
+      else if (p1Weapon === "Axe" && p2Weapon === "Axe") {
+          tie();
+      }
+
+      else if (p1Weapon === "Axe" && p2Weapon === "Lance") {
+        p1Win();
+        p2loss();
+
+      }
+
+      else if (p1Weapon === "Axe" && p2Weapon === "Sword") {
+        p2Win();
+        p1loss();
+      }
+
+       else if (p1Weapon === "Lance" && p2Weapon === "Lance") {
+          tie();
+      }
+
+      else if (p1Weapon === "Lance" && p2Weapon === "Sword") {
+        p1Win();
+        p2loss();
+      }
+
+      else if (p1Weapon === "Lance" && p2Weapon === "Axe") {
+        p2Win();
+        p1loss();
+      }
+};
 
 //once i figure out how to get p2 running need to make this for p2 as well
 playerChat.on("child_added", function(childSnapshot){
